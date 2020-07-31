54b9cde02cea689bb12c166cf41b65f5e7fd3ef3
yamanekaito <yamanekaito0702@gmail.com>
2020-08-01 05:12:13 +0900

    bingoのプログラムとボタン押すだけで正しいマスに色が付くようにしたい

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
     const column = [];
     const numbers = [];
     for (let i=0;i < 15;i++) {
-      numbers[i] = i + col*15;
+      numbers[i] = i + 1 + col*15;
     }
     for (let i=0;i < 5;i++) {
       column[i] = numbers.splice(Math.random() * numbers.length,1)[0];
@@ -12,24 +12,42 @@
     return column;
   }
 
-  const columns = [];
-  for (let i=0;i< 5;i++) {
-    columns[i] = createColumn(i);
-  }
-  columns[2][2] = 'free';
-  console.log(columns);
-
-
-  const tbody = document.querySelector('tbody');
-  for (let i=0;i<5;i++) {
-    const tr = document.createElement('tr');
-    for(let j=0;j<5;j++) {
-      const td = document.createElement('td');
-      td.textContent = columns[j][i];
-      tr.appendChild(td);
+  function createColumns() {
+    const columns = [];
+    for (let i=0;i< 5;i++) {
+      columns[i] = createColumn(i);
     }
-    tbody.appendChild(tr);
+    columns[2][2] = 'free';
+    return columns;
   }
+
+
+  function renderBingo(columns) {
+    const tbody = document.querySelector('tbody');
+    for (let i=0;i<5;i++) {
+      const tr = document.createElement('tr');
+      for(let j=0;j<5;j++) {
+        const td = document.createElement('td');
+        td.textContent = columns[j][i];
+        if(td.textContent === 'free') {
+          td.classList.add('active')
+        }else {
+          td.addEventListener('click',() => {
+            td.classList.toggle('active');
+          })
+        }
+        tr.appendChild(td);
+      }
+      tbody.appendChild(tr);
+    }
+
+  }
+
+  const columns = createColumns();
+  renderBingo(columns);
+
+
+
 
 
 }
